4997cd7901ba147f5f9608d1b1d03875320891c8
Keller Flint <kellerflint@Kellers-MacBook-Air.local>
2021-10-19 14:23:17 -0700

    added post-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mean-course';
+
+  posts: any = [
+    { title: "title 1", content: "Content 1" },
+    { title: "title 2", content: "Content 2" },
+    { title: "title 3", content: "Content 3" }
+  ];
+
+  onPostAdded(post: any) {
+    this.posts.push(post)
+  }
+
 }
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,16 +1,24 @@
-import { Component } from "@angular/core"
+import { Component, EventEmitter, Output } from "@angular/core"
 
 
 @Component({
     selector: 'post-create',
     templateUrl: './post-create.component.html',
+    styleUrls: ['./post-create.component.scss']
 })
 export class PostCreateComponent {
 
-    enteredValue: string = '';
-    newPost = "";
+    enteredTitle: string = '';
+    enteredContent: string = '';
 
-    savePost() {
-        this.newPost = this.enteredValue;
+    @Output()
+    postCreated = new EventEmitter();
+
+    onAddPost() {
+        const post = {
+            title: this.enteredTitle,
+            content: this.enteredContent
+        }
+        this.postCreated.emit(post);
     }
 }
